5c1ca649b501b9900a8f9cba38bf6f71b46f2436
mnurul <mnurulislam05@gmail.com>
2021-01-30 09:39:26 +0700

    Export database terbaru - DIMS

--- kelurahan.sql
+++ kelurahan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 07 Jan 2021 pada 16.20
+-- Waktu pembuatan: 30 Jan 2021 pada 03.38
 -- Versi server: 10.4.11-MariaDB
 -- Versi PHP: 7.4.6
 
@@ -20,6 +20,59 @@ SET time_zone = "+00:00";
 --
 -- Database: `kelurahan`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_file_rekap`
+--
+
+CREATE TABLE `tb_file_rekap` (
+  `id` int(11) NOT NULL,
+  `file_excel` varchar(100) NOT NULL,
+  `nik` varchar(16) NOT NULL,
+  `tgl_upload` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_file_rekap`
+--
+
+INSERT INTO `tb_file_rekap` (`id`, `file_excel`, `nik`, `tgl_upload`) VALUES
+(1, 'test2.xlsx', '3525100301770001', '2021-01-30'),
+(2, 'RW_022.xlsx', '3525100301770001', '2021-01-30'),
+(3, 'test3.xlsx', '3525100301770001', '2021-01-30'),
+(4, 'Report_Excel.xlsx', '3525100301770001', '2021-01-30'),
+(5, 'Report_Excel_(2).xlsx', '3525100301770001', '2021-01-30'),
+(6, 'Report_Excel_(2)1.xlsx', '3525100301770001', '2021-01-30'),
+(7, 'Report_Excel_(3).xlsx', '3525100301770001', '2021-01-30');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_rekap_data`
+--
+
+CREATE TABLE `tb_rekap_data` (
+  `id_rekap_data` int(11) NOT NULL,
+  `nik` varchar(16) NOT NULL,
+  `keterangan` text NOT NULL,
+  `status_rumah` varchar(30) NOT NULL,
+  `status_keluarga` varchar(60) NOT NULL,
+  `rt` varchar(3) NOT NULL,
+  `rw` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_rekap_data`
+--
+
+INSERT INTO `tb_rekap_data` (`id_rekap_data`, `nik`, `keterangan`, `status_rumah`, `status_keluarga`, `rt`, `rw`) VALUES
+(2, '3525100301770002', 'Sint nostrud volupta', 'Culpa quae est labor', 'Aut laborum voluptat', '002', '014'),
+(3, '3525100301770002', 'Voluptatum eos facil', 'Voluptas porro volup', 'Dolor ut quasi molli', '002', '014'),
+(4, '3525100301770002', 'Ex et repellendus E', 'Omnis sequi dolores ', 'Aliqua Voluptates a', '002', '014'),
+(5, '3525100301770002', 'Officiis accusamus t', 'In eveniet ea dolor', 'Veniam consequat N', '002', '014'),
+(6, '3525100301770002', 'Consectetur esse e123', 'Ad et animi aut qui', 'Velit rerum mollit r', '002', '014');
 
 -- --------------------------------------------------------
 
@@ -81,7 +134,7 @@ INSERT INTO `tb_surat_masuk_aparat` (`id_surat_masuk`, `id_surat`, `jenis_surat`
 CREATE TABLE `tb_surat_masuk_rt` (
   `id_surat_masuk` int(3) NOT NULL,
   `jenis_surat` varchar(30) NOT NULL,
-  `id_warga` varchar(16) NOT NULL,
+  `id_warga` varchar(16) DEFAULT NULL,
   `nik` varchar(16) NOT NULL,
   `no_surat_masuk` varchar(30) NOT NULL,
   `perihal` text NOT NULL,
@@ -100,7 +153,10 @@ CREATE TABLE `tb_surat_masuk_rt` (
 
 INSERT INTO `tb_surat_masuk_rt` (`id_surat_masuk`, `jenis_surat`, `id_warga`, `nik`, `no_surat_masuk`, `perihal`, `status_izin_rt`, `status_izin_rw`, `tgl_persetujuan`, `keterangan`, `tgl_persetujuan_rw`, `keterangan_rw`, `tgl_pengajuan`) VALUES
 (18, 'surat keterangan', '1', '3525100301770000', '', 'skck', 'disetujui', 'disetujui', '2021-01-09', 'ok', '2021-01-10', 'ok', '2021-01-09'),
-(20, 'surat keterangan', '', '3525100301770000', '', 'izin biasa', 'disetujui', 'disetujui', '2021-01-08', 'ok', '2021-01-08', 'ok', '2021-01-08');
+(20, 'surat keterangan', '', '3525100301770000', '', 'izin biasa', 'disetujui', 'disetujui', '2021-01-08', 'ok', '2021-01-08', 'ok', '2021-01-08'),
+(21, 'dukcapil', NULL, '', '', 'Eligendi porro repud', 'Belum disetujui', '', NULL, '', NULL, '', '2021-01-19'),
+(22, 'dukcapil', '1', '', '', 'Ut amet corporis do', 'Belum disetujui', '', NULL, '', NULL, '', '2021-01-19'),
+(23, 'surat keterangan', '1', '3525100301770000', '', 'Accusantium explicab', 'Belum disetujui', '', NULL, '', NULL, '', '2021-01-19');
 
 -- --------------------------------------------------------
 
@@ -181,11 +237,12 @@ CREATE TABLE `tb_user` (
 --
 
 INSERT INTO `tb_user` (`id_user`, `nik`, `nama`, `alamat`, `username`, `email`, `password`, `role_id`, `is_active`, `foto`, `date_created`) VALUES
-(10, '3525100301770000', '', '', 'dims', 'nurulraws@gmail.com', 'dims', '2', 1, '', '2021-01-02'),
-(11, '3525100301770001', '', '', 'Mian', 'mian@gmail.com', '1234', '3', 1, '', '2021-01-07'),
-(12, '3525100301770003', '', '', 'zafran', 'zafran@gmail.com', '1234', '4', 1, '', '2021-01-07'),
+(10, '3525100301770000', '', '', 'warga', 'nurulraws@gmail.com', 'dims', '2', 1, '', '2021-01-02'),
+(11, '3525100301770001', '', '', 'rw', 'mian@gmail.com', '1234', '3', 1, '', '2021-01-07'),
+(12, '3525100301770003', '', '', 'rt', 'zafran@gmail.com', '1234', '4', 1, '', '2021-01-07'),
 (13, '3525100301770002', '', '', 'aparat', 'aparat@gmail.com', '1234', '5', 1, '', '0000-00-00'),
-(14, '3525100301770004', '', '', 'admin', 'admin@gmail.com', 'admin', '1', 1, '', '0000-00-00');
+(14, '3525100301770004', '', '', 'admin', 'admin@gmail.com', 'admin', '1', 1, '', '0000-00-00'),
+(15, '3525100301770005', 'rt2', '', 'rt2', 'rt2@gmail.com', '1234', '4', 1, '', '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -214,11 +271,12 @@ CREATE TABLE `tb_warga` (
 --
 
 INSERT INTO `tb_warga` (`id_warga`, `nik`, `nama`, `tempat_lahir`, `tgl_lahir`, `alamat`, `agama`, `jenis_kelamin`, `pendidikan`, `pekerjaan`, `rt`, `rw`, `kelurahan`) VALUES
-(1, '3525100301770000', 'DIMS', 'Bekasi', '1998-02-01', 'Permata Harapan Baru Blok E1 Nomor 4 RT.002 RW.014, Pejuang, Medan Satria, Kota Bekasi, Pejuang, Medan Satria, Bekasi Barat, Jawa Barat 17131', 'Islam', 'L', 'SMK', 'wiraswasta', '002', '011', 'Medan Satria'),
+(1, '3525100301770000', 'DIMS', 'Bekasi', '1998-02-01', 'Permata Harapan Baru Blok E1 Nomor 4 RT.002 RW.014, Pejuang, Medan Satria, Kota Bekasi, Pejuang, Medan Satria, Bekasi Barat, Jawa Barat 17131', 'Islam', 'L', 'SMK', 'wiraswasta', '002', '014', 'Medan Satria'),
 (2, '3525100301770001', 'Della Rossa', 'Bekasi', '1999-03-10', 'Permata Harapan Baru Blok A1 Nomor 40 RT.002 RW.014, Pejuang, Medan Satria, Kota Bekasi, Pejuang, Medan Satria, Bekasi Barat, Jawa Barat 17131', 'Islam', 'P', 'SMA', 'Ibu Rumah Tangga', '001', '014', 'Medan Satria'),
 (3, '3525100301770002', 'Erik Setiawan', 'Bekasi', '1997-10-10', 'Permata Harapan Baru Blok D1 Nomor 10 RT.002 RW.014, Pejuang, Medan Satria, Kota Bekasi, Pejuang, Medan Satria, Bekasi Barat, Jawa Barat 17131', 'Katolik', 'L', 'SMA', 'wiraswasta', '002', '014', 'Medan Satria'),
-(4, '3525100301770003', 'Zafran', 'Jakarta', '2021-01-03', 'jln bima', 'Islam', 'L', '', '', '', '', ''),
-(5, '3525100301770004', 'admin', 'Jakarta', '2021-01-01', 'jln. bima terus 3', 'Islam', 'L', '', '', '', '', '');
+(4, '3525100301770003', 'Zafran', 'Jakarta', '2021-01-03', 'jln bima', 'Islam', 'L', '', '', '002', '014', ''),
+(5, '3525100301770004', 'admin', 'Jakarta', '2021-01-01', 'jln. bima terus 3', 'Islam', 'L', '', '', '001', '014', ''),
+(6, '3525100301770005', 'rt2', 'jakarta', '2021-01-20', '', '', 'L', '', '', '001', '014', '');
 
 -- --------------------------------------------------------
 
@@ -247,6 +305,18 @@ INSERT INTO `user_token` (`id`, `email`, `token`, `date_created`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `tb_file_rekap`
+--
+ALTER TABLE `tb_file_rekap`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tb_rekap_data`
+--
+ALTER TABLE `tb_rekap_data`
+  ADD PRIMARY KEY (`id_rekap_data`);
 
 --
 -- Indeks untuk tabel `tb_role`
@@ -301,6 +371,18 @@ ALTER TABLE `user_token`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `tb_file_rekap`
+--
+ALTER TABLE `tb_file_rekap`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_rekap_data`
+--
+ALTER TABLE `tb_rekap_data`
+  MODIFY `id_rekap_data` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT untuk tabel `tb_role`
 --
 ALTER TABLE `tb_role`
@@ -316,7 +398,7 @@ ALTER TABLE `tb_surat_masuk_aparat`
 -- AUTO_INCREMENT untuk tabel `tb_surat_masuk_rt`
 --
 ALTER TABLE `tb_surat_masuk_rt`
-  MODIFY `id_surat_masuk` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id_surat_masuk` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_surat_masuk_rw`
@@ -334,13 +416,13 @@ ALTER TABLE `tb_surat_rt`
 -- AUTO_INCREMENT untuk tabel `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `id_user` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_user` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_warga`
 --
 ALTER TABLE `tb_warga`
-  MODIFY `id_warga` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_warga` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `user_token`
